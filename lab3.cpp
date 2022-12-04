1fbfaa90474fdf4ba34f7aeb5e38b16fe4bb9711
Breden <ebrede2@uic.edu>
2022-12-03 21:06:45 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -16,38 +16,22 @@ void get_dim_counts(int m, MPI_Comm comm, int* dims_arr) {
 int main(int argc, char** argv) {    
     int this_rank;
     int num_procs;
-    // int dims[2] = {0, 0};
-    // int dim_counts[2];
-    // int periods[2] = {true, true};
-    // int this_coord[2];
-    // int neighbors[4] = {};
     int n = stoi(argv[1]);
-    // int k = stoi(argv[2]);
     int sub_n;
     int serial_result;
     double start;
-    // MPI_Request req;
     MPI_Status stat;
     Matrix X;
-
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &this_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    // MPI_Dims_create(num_procs, 2, dims);
-    // MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, true, &cart_comm);
-
-    // get new rank, cart coords, and amount of procs in each dim
-    // MPI_Comm_rank(cart_comm, &this_rank);
-    // MPI_Cart_coords(cart_comm, this_rank, 2, this_coord);
-    // get_dim_counts(2, cart_comm, dim_counts);
-
     if (this_rank == 0) {
         // create initial randomized matrix of size n
         X = Matrix(n);
         X.fill_rand(1);
-        // X.print();
+        X.print();
         start = MPI_Wtime();
 
         // algo
@@ -88,7 +72,6 @@ int main(int argc, char** argv) {
                 // ensure buffers copied before they go out of scope
                 MPI_Wait(&req1, &stat);
                 MPI_Wait(&req2, &stat);
-                }
             }
             cout << endl;
 
