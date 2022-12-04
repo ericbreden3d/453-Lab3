20653edea70f3e62f060d914b66f6a6b4f70448c
Breden <ebrede2@uic.edu>
2022-12-03 20:32:12 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -106,17 +106,17 @@ int main(int argc, char** argv) {
                 if (recv_proc != this_rank) {
                     continue;
                 }
-                
+                if (this_rank == 3) cout << "doing row 3\n\n";
                 // receive base row for iteration i if haven't already
                 if (k < num_procs) {
                     MPI_Recv(base_buf, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &stat);
-                    // cout << "->";
-                    // if (this_rank == 3)
-                    //     for (int j = 0; j < n; j++) {
-                    //         cout << base_buf[j] << " ";
-                    //     }
-                    //     cout << endl;
-                    // }
+                    cout << "->";
+                    if (this_rank == 3)
+                        for (int j = 0; j < n; j++) {
+                            cout << base_buf[j] << " ";
+                        }
+                        cout << endl;
+                    }
                 }
 
                 // receicve cur row k
@@ -127,12 +127,12 @@ int main(int argc, char** argv) {
                     // calc and send back
 
                     // debug
-                    // if (this_rank == 3)
-                    //     for (int j = 0; j < n; j++) {
-                    //         cout << cur_buf[j] << " ";
-                    //     }
-                    //     cout << endl;
-                    // }
+                    if (this_rank == 3)
+                        for (int j = 0; j < n; j++) {
+                            cout << cur_buf[j] << " ";
+                        }
+                        cout << endl;
+                    }
                 }
             }
         }
