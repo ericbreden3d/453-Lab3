f17cc53c390bdb039a2640e63423fe563ef7cf43
Breden <ebrede2@uic.edu>
2022-12-03 21:47:41 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
     if (this_rank == 0) {
         // create initial randomized matrix of size n
         Matrix A(n);
-        A.fill_rand();
+        A.fill_rand(-1);
         A.print();
         float serial_result = A.determinant();
         start = MPI_Wtime();
