cc1990e421676bf5f9a97ca75d382220c09874fb
Breden <ebrede2@uic.edu>
2022-12-06 02:20:16 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -166,7 +166,9 @@ int main(int argc, char** argv) {
 
                 // receicve cur row k
                 float cur_buf[n];
+                cout << "child " << this_rank << " waiting" << endl;
                 MPI_Recv(cur_buf, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &stat);
+                cout << "child " << this_rank << " received" << endl;
 
                 // received already zeroed row, ignore
                 if (cur_buf[i] == 0) {
