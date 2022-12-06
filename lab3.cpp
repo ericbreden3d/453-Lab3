44697875adf4a9c7220425166012a7de5baccbf9
Breden <ebrede2@uic.edu>
2022-12-05 23:01:36 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -147,7 +147,7 @@ int main(int argc, char** argv) {
                 }
                             
                 // receive base row for iteration i if haven't already
-                if (recv_proc <= num_procs) {
+                if (k <= num_procs) {
                     cout << "child waiting " << this_rank << " on base " << i << " for k: " << k << endl;
                     MPI_Recv(base_buf, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &stat);
                 }
