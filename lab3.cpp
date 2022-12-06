a4cefcb64d9fe84ab41c192bf997398c25457d03
Breden <ebrede2@uic.edu>
2022-12-06 17:44:42 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -151,8 +151,8 @@ int main(int argc, char** argv) {
                 // async receicve cur row k from root
                 // float cur_buf[n];
                 MPI_Request rreq;
-                MPI_Irecv(my_data[k], n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &reqs[k]);
-                MPI_Wait(&reqs[k], &stat);
+                MPI_Recv(my_data[k], n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &stat);
+                // MPI_Wait(&reqs[k], &stat);
 
                 if (cur_buf[i] == 0) {
                     continue;
