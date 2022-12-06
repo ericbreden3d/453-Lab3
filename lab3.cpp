0f6c3b3390fe68c474009c846aff97d00800a004
Breden <ebrede2@uic.edu>
2022-12-05 23:30:41 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -40,7 +40,7 @@ int main(int argc, char** argv) {
         Matrix A(n);
         A.fill_rand(-1);
         A.print();
-        float serial_result = A.determinant();
+        // float serial_result = A.determinant();
         start = MPI_Wtime();
 
         // algo
@@ -128,11 +128,11 @@ int main(int argc, char** argv) {
         // L.print();
         // A.print();
         // float det_L = L.determinant();
-        float det_U = A.determinant();
-        cout << "Serial Result: " << serial_result << endl;
+        // float det_U = A.determinant();
+        // cout << "Serial Result: " << serial_result << endl;
         // cout << "Parallel Result: " << det_L * det_U << endl;
         // cout << "Determinant of L: " << det_L << endl;
-        cout << "Determinant of U: " << det_U << endl;
+        // cout << "Determinant of U: " << det_U << endl;
         cout << "Parallel runtime: " << MPI_Wtime() - start << endl;
 
     } else {
