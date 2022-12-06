f7f3e2480e46743623e9cd7c0c2f9c29410652db
Breden <ebrede2@uic.edu>
2022-12-05 22:21:23 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -81,7 +81,6 @@ int main(int argc, char** argv) {
                 MPI_Wait(&req2, &stat);
             }
 
-
             // loop -> recv and update
             for (int k = i + 1; k < n; k++) {
                 if (A(i, k) == 0) {
@@ -103,6 +102,18 @@ int main(int argc, char** argv) {
                 // Then set to 0 in row and add row to U (A becomes U)
                 update_row(i, k, n, cur_buf, L, A);
                 
+                // L.print();
+                // A.print();
+            }
+
+            // root calculations
+            for (int j = 0; j < root_ind; j++) {
+                int k = root_rows[j];
+                float cur_buf[n];
+                A.get_row(k, cur_buf);
+                calc_row(i, n, base_buf, cur_buf);
+                update_row(i, k, n, cur_buf, L, A);
+
                 // L.print();
                 // A.print();
             }
