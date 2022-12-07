30fb4a43fef9094e839c4755ad46e15339bd844c
Breden <ebrede2@uic.edu>
2022-12-06 18:34:51 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -150,7 +150,6 @@ int main(int argc, char** argv) {
                 // receicve cur row k
                 // float cur_buf[n];
                 // cout << "child " << this_rank << " waiting " << i << ",s " << k << endl;
-                reqs[k] = MPI_Request();
                 MPI_Irecv(my_data[k], n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &reqs[k]);
                 // MPI_Wait(&reqs[k], &stat);
                 // cout << "child " << this_rank << " received " << i << ", " << k << endl;
