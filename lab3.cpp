6f1148bef1b507ac8cc19b168a886dbc3309f6e1
Breden <ebrede2@uic.edu>
2022-12-06 20:13:14 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -143,26 +143,9 @@ int main(int argc, char** argv) {
                 // cout << "here" << endl;
                 // reqs[k] = MPI_Request();
                 MPI_Irecv(my_data[k], n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &reqs[k]);
-                // MPI_Wait(&reqs[k], &stat);
+                MPI_Wait(&reqs[k], &stat);
 
                 // cout << "child " << this_rank << " received " << i << ", " << k << endl;
-
-                // received already zeroed row, ignore
-                
-                // if (my_data[k][i] == 0) {
-                //     continue;
-                // }
-
-                // // calculate multiplier, subtract row, and send back
-                // calc_row(i, n, base_buf, my_data[k]);
-                
-                // MPI_Request req;
-                // MPI_Send(my_data[k], n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-            }
-
-            for (int j = 0; j < my_ind; j++) {
-                int k = my_rows[j];
-                MPI_Wait(&reqs[k], &stat);
 
                 // received already zeroed row, ignore
                 if (my_data[k][i] == 0) {
@@ -172,8 +155,24 @@ int main(int argc, char** argv) {
                 // calculate multiplier, subtract row, and send back
                 calc_row(i, n, base_buf, my_data[k]);
                 
+                MPI_Request req;
                 MPI_Send(my_data[k], n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
             }
+
+            // for (int j = 0; j < my_ind; j++) {
+            //     int k = my_rows[j];
+            //     // MPI_Wait(&reqs[k], &stat);
+
+            //     // received already zeroed row, ignore
+            //     if (my_data[k][i] == 0) {
+            //         continue;
+            //     }
+
+            //     // calculate multiplier, subtract row, and send back
+            //     calc_row(i, n, base_buf, my_data[k]);
+                
+            //     MPI_Send(my_data[k], n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+            // }
 
             // MPI_Barrier(MPI_COMM_WORLD);
         }
