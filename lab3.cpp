278c341a24278f87dcbdc38f9e9653926c0a5742
Breden <ebrede2@uic.edu>
2022-12-06 18:20:58 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -54,17 +54,16 @@ int main(int argc, char** argv) {
 
             // distribute
             int root_rows[n - 1] = {};
-            // int base_sent[num_procs] = {};
             int root_ind = 0;
+            int child_rows[n - 1];
+            int child_ind = 0;
+            float child_data[n - 1][n];
             for (int k = i + 1; k < n; k++) {
 
                 int dest = (k - 1) % num_procs;
 
                 // if root's responsibility, store k
                 if (dest == 0) {
-                    if (A(k, i) == 0) {
-                        continue;
-                    }
                     root_rows[root_ind++] = k;
                     continue;
                 }
@@ -73,30 +72,30 @@ int main(int argc, char** argv) {
                 A.get_row(k, cur_buf);
 
                 // send row i if haven't sent already then current row k
-                MPI_Request req1, req2;
-                // if (!base_sent[dest]) {
-                //     MPI_Isend(base_buf, n, MPI_FLOAT, dest, 0, MPI_COMM_WORLD, &req1);
-                //     base_sent[dest] = 1;
-                // }
-                MPI_Isend(cur_buf, n, MPI_FLOAT, dest, 0, MPI_COMM_WORLD, &req2);
+                MPI_Request req;
+                MPI_Isend(cur_buf, n, MPI_FLOAT, dest, 0, MPI_COMM_WORLD, &req);
                 
                 // ensure buffers copied before they go out of scope
-                // MPI_Wait(&req1, &stat);
-                MPI_Wait(&req2, &stat);
+                MPI_Wait(&req, &stat);
             }
+
+            // async recv using child_rows gathered in last block
+            // for (int j = 0; j < root_ind; j++) {
+            //     int k = root_rows[j];
+            //     int src = (k - 1) % num_procs;
+
+            //     // receive response from child with subtracted row and multiplier at i.
+            //     // if ith elem was already 0, child is set to not respond
+            //     if (A(k, i) != 0) {
+            //         // MPI_Irecv(child_data[k], n, MPI_FLOAT, src, 0, MPI_COMM_WORLD, &reqs[k]);
+            //         MPI_Recv(child_data[k], n, MPI_FLOAT, src, 0, MPI_COMM_WORLD, &stat);
+            //     }
+            // }
 
             // loop -> recv and update
             for (int k = i + 1; k < n; k++) {
-                // first elem of row sent to child already 0, so it will not respond
-                if (A(k, i) == 0) {
-                    // cout << "ZERO" << endl;
-                    continue;
-                }
-
                 int dest = (k - 1) % num_procs;
-
-                // if root calculated this root, ignore
-                if (dest == 0) {
+                if (A(k, i) == 0 || dest == 0) {
                     continue;
                 }
                 
@@ -116,8 +115,10 @@ int main(int argc, char** argv) {
                 int k = root_rows[j];
                 float cur_buf[n];
                 A.get_row(k, cur_buf);
-                calc_row(i, n, base_buf, cur_buf);
-                update_row(i, k, n, cur_buf, L, A);
+                if (cur_buf[i] != 0) {
+                    calc_row(i, n, base_buf, cur_buf);
+                    update_row(i, k, n, cur_buf, L, A);
+                }
             }
         }
 
