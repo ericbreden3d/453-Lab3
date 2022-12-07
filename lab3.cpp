92df8f996ccb43aa9a328a7c0832227d00b9e1d5
Breden <ebrede2@uic.edu>
2022-12-06 18:05:22 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -39,39 +39,32 @@ int main(int argc, char** argv) {
         cout << "n = " << n << endl;
         Matrix A(n);
         A.fill_rand(-1);
-        // A.print();
         Matrix U = A;
+        // A.print();
         start = MPI_Wtime();
 
         // algo
         Matrix L(n);
         for (int i = 0; i < n - 1; i++) {
+            
             // get base row i for this iter
             float base_buf[n];
             A.get_row(i, base_buf);
-            if (base_buf[i] == 0) {
-                cout << "BAD ZERO" << endl;
-            }
+            MPI_Bcast(base_buf, n, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
             // distribute
             int root_rows[n - 1] = {};
-            int base_sent[num_procs] = {};
+            // int base_sent[num_procs] = {};
             int root_ind = 0;
             for (int k = i + 1; k < n; k++) {
-                // if (A(i, k) == 0) {
-                //     cout << "ZERO" << endl;
-                //     continue;
-                // } 
 
                 int dest = (k - 1) % num_procs;
 
                 // if root's responsibility, store k
                 if (dest == 0) {
                     if (A(k, i) == 0) {
-                        // cout << "root zero condition" << endl;
                         continue;
                     }
-                    // cout << "root responsible for " << k << endl;
                     root_rows[root_ind++] = k;
                     continue;
                 }
@@ -81,10 +74,10 @@ int main(int argc, char** argv) {
 
                 // send row i if haven't sent already then current row k
                 MPI_Request req1, req2;
-                if (!base_sent[dest]) {
-                    MPI_Isend(base_buf, n, MPI_FLOAT, dest, 0, MPI_COMM_WORLD, &req1);
-                    base_sent[dest] = 1;
-                }
+                // if (!base_sent[dest]) {
+                //     MPI_Isend(base_buf, n, MPI_FLOAT, dest, 0, MPI_COMM_WORLD, &req1);
+                //     base_sent[dest] = 1;
+                // }
                 MPI_Isend(cur_buf, n, MPI_FLOAT, dest, 0, MPI_COMM_WORLD, &req2);
                 
                 // ensure buffers copied before they go out of scope
@@ -96,7 +89,7 @@ int main(int argc, char** argv) {
             for (int k = i + 1; k < n; k++) {
                 // first elem of row sent to child already 0, so it will not respond
                 if (A(k, i) == 0) {
-                    cout << "ZERO" << endl;
+                    // cout << "ZERO" << endl;
                     continue;
                 }
 
@@ -116,32 +109,17 @@ int main(int argc, char** argv) {
                 // update L with multiplier stored at row[i].
                 // Then set to 0 in row and add row to U (A becomes U)
                 update_row(i, k, n, cur_buf, L, A);
-                
-                // L.print();
-                // A.print();
             }
 
             // root calculations (maybe move above receives)
             for (int j = 0; j < root_ind; j++) {
                 int k = root_rows[j];
-                // cout << "k: " << k << endl;
                 float cur_buf[n];
                 A.get_row(k, cur_buf);
                 calc_row(i, n, base_buf, cur_buf);
                 update_row(i, k, n, cur_buf, L, A);
-
-                // L.print();
-                // A.print();
             }
         }
-
-        // add 1s to diagonal of L
-        // for (int i = 0; i < n; i++) {
-        //     L(i, i) = 1;
-        // }
-
-        // L.print();
-        // A.print();
 
         // float serial_result = U.determinant();
         // float parallel_result = A.determinant();
@@ -153,7 +131,8 @@ int main(int argc, char** argv) {
         // child logic
         for (int i = 0; i < n - 1; i++) {
             float base_buf[n];
-            int base_recvd = 0;
+            MPI_Bcast(base_buf, n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+            // int base_recvd = 0;
             for (int k = i + 1; k < n; k++) {
                 // dont't proceed unless this proc is needed
                 int recv_proc = (k - 1) % num_procs;
@@ -162,12 +141,12 @@ int main(int argc, char** argv) {
                 }
                             
                 // receive base row for iteration i if haven't already
-                if (!base_recvd) {
-                    // cout << "child waiting " << this_rank << " on base " << i << " for " << i << ", " << k << endl;
-                    MPI_Recv(base_buf, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &stat);
-                    // cout << "child " << this_rank << " received " << k << endl;
-                    base_recvd = 1;
-                }
+                // if (!base_recvd) {
+                //     // cout << "child waiting " << this_rank << " on base " << i << " for " << i << ", " << k << endl;
+                //     MPI_Recv(base_buf, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &stat);
+                //     // cout << "child " << this_rank << " received " << k << endl;
+                //     base_recvd = 1;
+                // }
 
                 // receicve cur row k
                 float cur_buf[n];
