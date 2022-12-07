706ed113f7f14dbe39ab885a745ad03fc32937a0
Breden <ebrede2@uic.edu>
2022-12-06 19:41:13 -0600

    update

--- lab3.cpp
+++ lab3.cpp
@@ -125,7 +125,7 @@ int main(int argc, char** argv) {
 
             int my_rows[n - 1];
             int my_ind = 0;
-            float my_data[n - 1][n];
+            float my_data[n][n];
             MPI_Request reqs[n];
             for (int k = i + 1; k < n; k++) {
                 // dont't proceed unless this proc is needed
